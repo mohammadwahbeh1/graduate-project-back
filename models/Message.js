6dd33f4a3ee6858ca0328141c3e255fae7db5d07
Abbas-Nedal <s12011080@stu.najah.edu>
2025-01-16 17:31:54 +0200

    edit  some conf to support emoji

--- models/Message.js
+++ models/Message.js
@@ -43,6 +43,8 @@ const Message = sequelize.define('Message', {
 }, {
     tableName: 'Messages',
     timestamps: true,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci'
 });
 
 // Associations
